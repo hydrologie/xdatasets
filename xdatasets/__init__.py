929be1d47d3bb98abad035723d7f7b36277a8196
sebastienlanglois <sebastien.langlois@polymtl.ca>
2023-03-21 09:44:32 -0400

    bump version

--- xdatasets/__init__.py
+++ xdatasets/__init__.py
@@ -1,2 +1,6 @@
 from .tutorial import load_dataset, list_available_datasets
 from .core import Query
+
+__author__ = """Sebastien Langlois"""
+__email__ = "sebastien.langlois62@gmail.com"
+__version__ = "0.1.2-alpha"
