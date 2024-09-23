82f48b46803846c4d40cfe6ee0420ce0ce3bd51a
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2024-09-23 12:00:56 -0400

    use GitHub Helper bot for bumpversion workflow, remove obsolete workflow, fast-forward cookiecutter

--- docs/conf.py
+++ docs/conf.py
@@ -67,10 +67,8 @@ nbsphinx_execute = "always"
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = [".rst"]
+# You can specify multiple suffix as a dictionary of suffix: filetype
+source_suffix = {".rst": "restructuredtext"}
 
 # The master toctree document.
 master_doc = "index"
