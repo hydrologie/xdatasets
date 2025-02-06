b849166ce27441813f4e32496c9ffbc61c523fc8
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2025-02-06 15:30:10 -0500

    update cookiecutter version

--- tests/test_xdatasets.py
+++ tests/test_xdatasets.py
@@ -4,32 +4,36 @@
 import pathlib
 from importlib.util import find_spec
 
-import pytest
+# import pytest
 
 import xdatasets  # noqa: F401
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
+#
+#     See more at: https://doc.pytest.org/en/latest/explanation/fixtures.html
+#     """
+#     # import requests
+#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-    See more at: https://doc.pytest.org/en/latest/explanation/fixtures.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+# def test_content(response):
+#     """Sample pytest test function with the pytest fixture as an argument."""
+#     # from bs4 import BeautifulSoup
+#     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
 def test_package_metadata():
     """Test the package metadata."""
-    project = find_spec("xdatasets").submodule_search_locations[0]
+    project = find_spec("xdatasets")
 
-    metadata = pathlib.Path(project).resolve().joinpath("__init__.py")
+    assert project is not None
+    assert project.submodule_search_locations is not None
+    location = project.submodule_search_locations[0]
+
+    metadata = pathlib.Path(location).resolve().joinpath("__init__.py")
 
     with metadata.open() as f:
         contents = f.read()
